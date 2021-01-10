211b97582ff243dec6f7c9a3065e5c41db7bd9d8
MarkoKlin <klincharov_marko@hotmail.com>
2021-01-11 00:39:13 +0100

    Basic structure and description.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>As yet unnamed</title>
+</head>
+<body>
+    <?php 
+        
+    ?>
+</body>
+</html>
