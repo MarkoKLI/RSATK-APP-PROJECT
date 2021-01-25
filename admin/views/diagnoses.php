a666c7c667ac97010d1da95213d5a2132dd62d75
MarkoKlin <klincharov_marko@hotmail.com>
2021-01-25 23:26:17 +0100

    Finished views w/o php & validation

--- admin/views/diagnoses.php
+++ admin/views/diagnoses.php
@@ -1,3 +1,143 @@
-<h1>
-    diagnoses
-</h1>
+<div class="card container-card">
+    <div class="card-header">
+        <h2>Дијагнози</h2>
+    </div>
+    <div id="accordion" class="card-body">
+        <div class="row form-group justify-content-center">
+            <label class="col-form-label col-2" for="search-diagnoses">Пребарај дијагноза:</label>
+            <div class="col-10">
+                <input type="search" name="search-diagnoses" id="search-diagnoses"
+                        placeholder="Search" class="form-control">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="title1">
+                <div class="row justify-content-between">
+                    <button class="btn col-lg-9 ml-2" data-toggle="collapse" data-target="#desc1" 
+                            aria-expanded="true" aria-controls="desc1">
+                    Кардиологија
+                    </button>
+                    <div class="row col-lg-3">
+                        <form action="<?php echo $location; ?>" method="post" class="mr-4">
+                            <input type="hidden" name="action" value="edit_department">
+                            <input class="btn btn-outline-primary" type="submit"
+                                    value="Промени">
+                        </form>
+                        <form action="<?php echo $location; ?>" method="post">
+                            <input type="hidden" name="action" value="delete_department">
+                            <input class="btn btn-outline-danger" type="submit"
+                                    value="Избриши">
+                        </form>
+                </div>
+                </div>
+            </div>
+            <div id="desc1" class="collapse show" aria-labelledby="title1" 
+                data-parent="#accordion">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="title2">
+                <div class="row justify-content-between">
+                    <button class="btn collapsed col-lg-9 ml-2" data-toggle="collapse" data-target="#desc2" aria-expanded="false" aria-controls="desc2">
+                    Онкологија
+                    </button>
+                    <div class="row col-lg-3">
+                        <form action="<?php echo $location; ?>" method="post" class="mr-4">
+                            <input type="hidden" name="action" value="edit_department">
+                            <input class="btn btn-outline-primary" type="submit"
+                                    value="Промени">
+                        </form>
+                        <form action="<?php echo $location; ?>" method="post">
+                            <input type="hidden" name="action" value="delete_department">
+                            <input class="btn btn-outline-danger" type="submit"
+                                    value="Избриши">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div id="desc2" class="collapse" aria-labelledby="title2" data-parent="#accordion">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="title3">
+                <div class="row justify-content-between">
+                    <button class="btn collapsed col-lg-9 ml-2" data-toggle="collapse" data-target="#desc3" aria-expanded="false" aria-controls="desc3">
+                    Нефрологија
+                    </button>
+                    <div class="row col-lg-3">
+                        <form action="<?php echo $location; ?>" method="post" class="mr-4">
+                            <input type="hidden" name="action" value="edit_department">
+                            <input class="btn btn-outline-primary" type="submit"
+                                    value="Промени">
+                        </form>
+                        <form action="<?php echo $location; ?>" method="post">
+                            <input type="hidden" name="action" value="delete_department">
+                            <input class="btn btn-outline-danger" type="submit"
+                                    value="Избриши">
+                        </form>
+                </div>
+                </div>
+            </div>
+            <div id="desc3" class="collapse" aria-labelledby="title3" data-parent="#accordion">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card container-card">
+    <div class="card-header">
+        <h3>Креирај дијагноза: </h3>
+    </div>
+    <div class="card-body">
+        <form class="needs-validation" action="<?php echo $location; ?>" method="post" novalidate>
+            <input type="hidden" name="action" value="create_diagnosis">
+            <div class="form-group row">
+                <label for="title" class="col-2 col-form-label text-center">Назив на дијагноза: </label>
+                <div class="col-10">
+                    <input class="form-control" id="title" name="title" 
+                        placeholder="Назив" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="desc" class="col-2 col-form-label text-center">Опис на дијагноза: </label>
+                <div class="col-10">
+                    <textarea class="form-control" id="desc" name="desc" 
+                        placeholder="Опис..." cols="30" required></textarea>
+                </div>
+            </div>
+            <input class="btn btn-outline-primary" type="submit" value="Креирај!">
+        </form>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        $("#search-diagnoses").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#accordion .card-header").filter(function() {
+                    $(this).parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+
+        'use strict';
+        window.addEventListener('load', function() {
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    });
+</script>
--- admin/views/users.php
+++ admin/views/users.php
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="card container-card">
     <div class="card-header">
         <h2>Корисници</h2>
     </div>
@@ -10,38 +10,61 @@
                         placeholder="Search" class="form-control">
             </div>
         </div>
-        <table id="user-table" class="table table-hover">
-            <tr>
-                <th>id</th>
-                <th>Име и презиме</th>
-                <th>Тип на корисник</th>
-                <th>Избриши</th>
-            </tr>
-            <tr>
-                <th>1</th>
-                <th>John Doe</th>
-                <th>Doctor</th>
-                <th>x</th>
-            </tr>
-            <tr>
-                <th>2</th>
-                <th>John Doe</th>
-                <th>Doctor</th>
-                <th>x</th>
-            </tr>
-            <tr>
-                <th>3</th>
-                <th>John Doe</th>
-                <th>Doctor</th>
-                <th>x</th>
-            </tr>
-        </table>
+        <div class="table-container">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Име и презиме</th>
+                        <th>Тип на корисник</th>
+                        <th>Избриши</th>
+                    </tr>
+                </thead>
+                <tbody id="table-users">
+                    <tr>
+                        <th>1</th>
+                        <th>Thirteen</th>
+                        <th>Doctor</th>
+                        <th>
+                            <form action="<?php echo $location; ?>" method="post">
+                                <input type="hidden" name="action" value="delete_user">
+                                <input type="hidden" name="userId" value="1">
+                                <input class="btn btn-outline-danger btn-sm" type="submit" value="Бриши!">
+                            </form>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <th>Lisa Cuddy</th>
+                        <th>Patient</th>
+                        <th><form action="<?php echo $location; ?>" method="post">
+                                <input type="hidden" name="action" value="delete_user">
+                                <input type="hidden" name="userId" value="1">
+                                <input class="btn btn-outline-danger btn-sm" type="submit" value="Бриши!">
+                            </form>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>3</th>
+                        <th>Gregory House</th>
+                        <th>Doctor</th>
+                        <th>
+                            <form action="<?php echo $location; ?>" method="post">
+                                <input type="hidden" name="action" value="delete_user">
+                                <input type="hidden" name="userId" value="1">
+                                <input class="btn btn-outline-danger btn-sm" type="submit" value="Бриши!">
+                            </form>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
 <div class="container">
     <div class="row justify-content-between">
-        <div class="card create-user-card">
+        <div class="card container-card create-user-card">
             <div class="card-header">
                 <h3>Креирај корисник:</h3>
             </div>
@@ -51,45 +74,73 @@
                     <div class="form-group row">
                         <label for="name" class="col-4 col-form-label">Име: </label>
                         <div class="col-8">
-                            <input class="form-control" id="name" placeholder="Име">
+                            <input class="form-control" id="name" name="name" 
+                            placeholder="Име" required>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="surname" class="col-4 col-form-label">Презиме: </label>
                         <div class="col-8">
-                            <input class="form-control" id="surname" placeholder="Презиме">
+                            <input class="form-control" id="surname" name="surname" 
+                            placeholder="Презиме" required>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="embr" class="col-4 col-form-label">ЕМБР: </label>
                         <div class="col-8">
-                            <input class="form-control" id="embr" placeholder="ЕМБР">
+                            <input class="form-control" id="embr" name="embr" 
+                            placeholder="ЕМБР" required pattern="[0-9]{13}$">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="address" class="col-4 col-form-label">Адреса: </label>
+                        <div class="col-8">
+                            <input class="form-control" id="address" name="address"
+                            placeholder="Адреса" required>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="email" class="col-4 col-form-label">Email: </label>
                         <div class="col-8">
-                            <input class="form-control" type="email" id="email" placeholder="Email">
+                            <input class="form-control" type="email" id="email" name="email" 
+                            placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="phoneNr" class="col-4 col-form-label">Телефонски број: </label>
                         <div class="col-8">
-                            <input type="tel" class="form-control" id="phoneNr" 
-                            pattern="[0-9]{0,14}$" placeholder="Telephone number">
+                            <input type="tel" class="form-control" id="phoneNr" name="phoneNr"
+                            pattern="[0-9]{0,14}$" placeholder="Telephone number" required>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="dob" class="col-4 col-form-label">Датум на раѓање: </label>
                         <div class="col-8">
-                            <input class="form-control" type="date" id="dob">
+                            <input class="form-control" type="date" id="dob" name="dob" required>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="custom-control custom-switch col-8">
+                            <input type="checkbox" class="custom-control-input" id="isDoctor" value="true">
+                            <label class="custom-control-label" for="isDoctor">Докторски профил</label>
+                        </div>
+                    </div>
+                    <div id="specialtyDiv" class="form-group row">
+                        <label for="dob" class="col-4 col-form-label">Одделение: </label>
+                        <div class="col-8">
+                            <select class="form-control" name="specialty" id="specialty">
+                            <option value="ad">sad</option>
+                            <option value="ad">bad</option>
+                            <option value="ad">mad</option>
+                            <option value="ad">rad</option>
+                            </select>
                         </div>
                     </div>
                     <input class="btn btn-primary" type="submit" value="Креирај">
                 </form>
             </div>
         </div>
-        <div class="card create-user-card">
+        <div class="card container-card create-admin-card">
             <div class="card-header">
                 <h3>Креирај администратор: </h3>
             </div>
@@ -121,7 +172,14 @@
     </div>
 </div>
 <script>
-    (function() {
+    $(document).ready(function(){
+        $("#search-users").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#table-users tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+
         'use strict';
         window.addEventListener('load', function() {
             var forms = document.getElementsByClassName('needs-validation');
@@ -135,5 +193,11 @@
                 }, false);
             });
         }, false);
-    })();
+
+        $("#specialtyDiv").hide();
+        $("#isDoctor").click(function() {
+            $("#specialtyDiv").toggle();
+            console.log("click");
+        })
+    });
 </script>
