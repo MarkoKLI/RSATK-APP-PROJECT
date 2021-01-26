3bd0278ab3329d085d8ccb0ba3cc4d57add18668
Tea Dimitrovska <tea.dimitrovska@outlook.com>
2021-01-26 20:27:15 +0100

    hhh

--- index.php
+++ index.php
@@ -16,6 +16,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>As yet unnamed</title>
+    
+    <link rel="stylesheet" href="styles/styles.css">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
 </head>
 <body>
     <h3> РСАТК ПРОЕКТ АПЛИКАЦИЈА </h3>
