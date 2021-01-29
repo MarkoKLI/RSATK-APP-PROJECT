a8af2a48de655172cda71e9b540b7e1ca1fa7d81
Teodora Kolevska <teodorakolevska13@gmail.com>
2021-01-29 20:45:42 +0100

    error i footer

---
+++ views/footer.php
@@ -0,0 +1,49 @@
+<footer class="page-footer font-small elegant-color">
+<div class="container">
+
+    <div class="row">
+
+      <!--Grid column-->
+      <div class="col my-5">
+
+        <!-- Social buttons -->
+        <ul class="list-unstyled list-inline text-right mb-0">
+          <li class="list-inline-item">
+            <a class="btn-floating btn-fb mx-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-tw mx-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-gplus mx-1">
+              <i class="fab fa-google-plus-g"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-li mx-1">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-dribbble mx-1">
+              <i class="fab fa-dribbble"></i>
+            </a>
+          </li>
+          <button type="button" class="btn btn-link-black-group-sm">Log in</button>
+        </ul>
+        <!-- Social buttons -->
+        
+      </div>
+      <!--Grid column-->
+
+    </div>
+    <!--Grid row-->
+
+  </div>
+
+
+</footer>
