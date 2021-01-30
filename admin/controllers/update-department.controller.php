cfb3ce1a20db0805839478ae1214ca1ad7d0d4b7
MarkoKlin <klincharov_marko@hotmail.com>
2021-01-30 20:35:29 +0100

    admin/department fix

--- admin/controllers/update-department.controller.php
+++ admin/controllers/update-department.controller.php
@@ -3,7 +3,7 @@
         $requestFailed = "Something went wrong. Please try again!";
     } else {
         $id = filter_input(INPUT_POST,"id",FILTER_VALIDATE_INT);
-        $name = ConversionService::SecureInput($_POST["title"]);
+        $title = ConversionService::SecureInput($_POST["title"]);
         $desc = ConversionService::SecureInput($_POST["desc"]);
         
         DepartmentsAdminService::updateDepartment($id, $title, $desc);
