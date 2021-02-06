8c3b01d2746ea7aa41006679821a2699d354818e
Tea Dimitrovska <tea.dimitrovska@outlook.com>
2021-02-06 12:48:29 +0100

    error update

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/error.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 </head>
